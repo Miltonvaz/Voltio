c826062cc407365647634e0a030755f6961a4c4e
Jaredtorr <233310@ids.upchiapas.edu.mx>
2026-02-24 22:39:50 -0600

    arnavcorrec

--- app/src/main/java/com/miltonvaz/voltio_1/features/auth/presentation/navigation/LoginNavGraph.kt
+++ app/src/main/java/com/miltonvaz/voltio_1/features/auth/presentation/navigation/LoginNavGraph.kt
@@ -1,6 +1,5 @@
 package com.miltonvaz.voltio_1.features.auth.presentation.navigation
 
-import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.composable
@@ -10,8 +9,6 @@ import com.miltonvaz.voltio_1.core.navigation.Login
 import com.miltonvaz.voltio_1.core.navigation.Register
 import com.miltonvaz.voltio_1.features.auth.presentation.screens.LoginScreen
 import com.miltonvaz.voltio_1.features.auth.presentation.screens.register.RegisterScreen
-import com.miltonvaz.voltio_1.features.auth.presentation.viewmodel.LoginViewModel
-import com.miltonvaz.voltio_1.features.auth.presentation.viewmodel.RegisterViewModel
 
 class LoginNavGraph : FeatureNavGraph {
     override fun registerGraph(
@@ -19,9 +16,7 @@ class LoginNavGraph : FeatureNavGraph {
         navController: NavHostController
     ) {
         navGraphBuilder.composable<Login> {
-            val viewModel: LoginViewModel = hiltViewModel()
             LoginScreen(
-                viewModel = viewModel,
                 onBackClick = { navController.popBackStack() },
                 onRegisterClick = { navController.navigate(Register) },
                 onLoginSuccess = {
@@ -33,9 +28,7 @@ class LoginNavGraph : FeatureNavGraph {
         }
 
         navGraphBuilder.composable<Register> {
-            val viewModel: RegisterViewModel = hiltViewModel()
             RegisterScreen(
-                viewModel = viewModel,
                 onBackClick = { navController.popBackStack() },
                 onRegisterSuccess = {
                     navController.navigate(Login) {
