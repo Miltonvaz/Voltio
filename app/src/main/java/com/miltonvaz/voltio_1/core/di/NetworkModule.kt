d6de66a88a76ef649a88e63983fbc146ff7c84ef
Jaredtorr <233310@ids.upchiapas.edu.mx>
2026-02-25 00:16:11 -0600

    qualifiers

--- app/src/main/java/com/miltonvaz/voltio_1/core/di/NetworkModule.kt
+++ app/src/main/java/com/miltonvaz/voltio_1/core/di/NetworkModule.kt
@@ -7,7 +7,6 @@ import com.miltonvaz.voltio_1.features.products.data.datasource.remote.ProductAp
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
